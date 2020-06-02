72a3a158b4abf226910766e895d706074480a326
Henrique Da Costa <henrique.dacosta@orange.fr>
2020-06-02 16:19:56 +0200

    Backend -> App - Routes - Controllers - Middlewares

---
+++ Back-end/routes/sauces.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const saucesCtrl = require('../controllers/sauces');
+
+
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.get('/', auth, saucesCtrl.getAllSauce);
+
+module.exports = router;
